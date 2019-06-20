b86d08c324ed70f7e0c10bdc57e9e1f1eb91ab58
IPlayers <max7338.m@gmail.com>
2019-06-20 16:15:48 +0200

    Preversion

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : { apiKey: 'AIzaSyD8LyxDrt0ijGjzux1_fTrgmCn-4O6lIq4',
+    authDomain: 'msprdev-f9a37.firebaseapp.com',
+    databaseURL: 'https://msprdev-f9a37.firebaseio.com',
+    projectId: 'msprdev-f9a37',
+    storageBucket: 'msprdev-f9a37.appspot.com',
+    messagingSenderId: '938087911424',
+    appId: '1:938087911424:web:8764f77d96ccf840' }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
